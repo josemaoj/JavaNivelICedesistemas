1431e69a243649b39ba3a374a4bbef0a2e2417ad
josemaoj <65726151+josemaoj@users.noreply.github.com>
2020-11-09 17:06:59 -0500

    Add files via upload

---
+++ Clase5 Parte1/src/Clase5/Clase5Test.java
@@ -0,0 +1,22 @@
+package Clase5;
+
+public class Clase5Test {
+	
+	public static void main(String[] args) {
+		System.out.println("Operación Suma");
+		Suma suma1=new Suma();
+	    suma1.cargar1();
+	    suma1.cargar2();
+	    suma1.operar();
+	    System.out.print("El resultado de la suma es:");
+	    suma1.mostrarResultado();
+	    System.out.println("Operación Resta");
+	    Resta resta1=new Resta();
+	    resta1.cargar1();
+	    resta1.cargar2();
+	    resta1.operar();
+	    System.out.print("El resultado de la resta es:");        
+	    resta1.mostrarResultado();
+	}
+
+}
---
+++ Clase5 Parte1/src/Clase5/PruebaP.java
@@ -0,0 +1,12 @@
+package Clase5;
+
+public class PruebaP {
+
+	public static void main(String[] args) {
+		Operacion o = new  Operacion();
+		o.cargar1();
+		o.getValor1();
+		//o.setValor1(valor1);
+	}
+	
+}
