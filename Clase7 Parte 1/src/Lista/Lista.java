3d42166248d0b463184b4f7c71daae17a7abb477
josemaoj <65726151+josemaoj@users.noreply.github.com>
2020-11-12 16:30:44 -0500

    Add files via upload

---
+++ Clase7 Parte 1/src/Lista/Lista.java
@@ -0,0 +1,204 @@
+package Lista;
+
+/***
+ * Clase para el manejo de Listas
+ * @author jose.jaramillo
+ * @since	11/11/2020
+ * @version 1
+ */
+public class Lista {
+	
+	/**
+	 * Clase nodo
+	 * @author jose.jaramillo
+	 * 
+	 */
+	class Nodo {
+        int info;
+        Nodo sig;
+    }
+    
+	//Crear un objeto nodo llamado raiz
+    private Nodo raiz;
+    
+    /**
+     * Constructor de la clase
+     */
+    public Lista() {
+        raiz=null; //Crea una lista vacia
+    }
+    
+    /**
+     * Metodo inserta nodos en una lista generica
+     * @param pos
+     * @param x
+     */
+    void insertar (int pos, int x)
+    {
+        //Si la posición es menor o igual que la cantidad de nodos de la lista
+    	if (pos <= cantidad () + 1)    {
+            Nodo nuevo = new Nodo ();
+            nuevo.info = x;
+            if (pos == 1){
+                nuevo.sig = raiz;
+                raiz = nuevo;
+            } else
+                if (pos == cantidad () + 1)    {
+                    Nodo reco = raiz;
+                    while (reco.sig != null) {
+                        reco = reco.sig;
+                    }
+                    reco.sig = nuevo;
+                    nuevo.sig = null;
+                } else {
+                    Nodo reco = raiz;
+                    for (int f = 1 ; f <= pos - 2 ; f++)
+                        reco = reco.sig;
+                    Nodo siguiente = reco.sig;
+                    reco.sig = nuevo;
+                    nuevo.sig = siguiente;
+                }
+        }
+    }
+
+    public int extraer (int pos) {
+        if (pos <= cantidad ())    {
+            int informacion;
+            if (pos == 1) {
+                informacion = raiz.info;
+                raiz = raiz.sig;
+            } else {
+                Nodo reco;
+                reco = raiz;
+                for (int f = 1 ; f <= pos - 2 ; f++)
+                    reco = reco.sig;
+                Nodo prox = reco.sig;
+                reco.sig = prox.sig;
+                informacion = prox.info;
+            }
+            return informacion;
+        }
+        else
+            return Integer.MAX_VALUE;
+    }
+
+    public void borrar (int pos)
+    {
+        if (pos <= cantidad ())    {
+            if (pos == 1) {
+                raiz = raiz.sig;
+            } else {
+                Nodo reco;
+                reco = raiz;
+                for (int f = 1 ; f <= pos - 2 ; f++)
+                    reco = reco.sig;
+                Nodo prox = reco.sig;
+                reco.sig = prox.sig;
+            }
+        }
+    }
+    
+    public void intercambiar (int pos1, int pos2) {
+        if (pos1 <= cantidad () && pos2 <= cantidad ())    {
+            Nodo reco1 = raiz;
+            for (int f = 1 ; f < pos1 ; f++)
+                reco1 = reco1.sig;
+            Nodo reco2 = raiz;
+            for (int f = 1 ; f < pos2 ; f++)
+                reco2 = reco2.sig;
+            int aux = reco1.info;
+            reco1.info = reco2.info;
+            reco2.info = aux;
+        }
+    }
+    
+    public int mayor () {
+        if (!vacia ()) {
+            int may = raiz.info;
+            Nodo reco = raiz.sig;
+            while (reco != null) {
+                if (reco.info > may)
+                    may = reco.info;
+                reco = reco.sig;
+            }
+            return may;
+        }
+        else
+            return Integer.MAX_VALUE;
+    }
+    
+    public int posMayor() {
+        if (!vacia ())    {
+            int may = raiz.info;
+            int x=1;
+            int pos=x;
+            Nodo reco = raiz.sig;
+            while (reco != null){
+                if (reco.info > may) {
+                    may = reco.info;
+                    pos=x;
+                }
+                reco = reco.sig;
+                x++;
+            }
+            return pos;
+        }
+        else
+            return Integer.MAX_VALUE;
+    }
+
+    public int cantidad ()
+    {
+        int cant = 0;
+        Nodo reco = raiz;
+        while (reco != null) {
+            reco = reco.sig;
+            cant++;
+        }
+        return cant;
+    }
+    
+    public boolean ordenada() {
+        if (cantidad()>1) {
+            Nodo reco1=raiz;
+            Nodo reco2=raiz.sig;
+            while (reco2!=null) {
+                if (reco2.info<reco1.info) {
+                    return false;
+                }
+                reco2=reco2.sig;
+                reco1=reco1.sig;
+            }
+        }
+        return true;
+    }
+    
+    public boolean existe(int x) {
+        Nodo reco=raiz;
+        while (reco!=null) {
+            if (reco.info==x)
+                return true;
+            reco=reco.sig;
+        }
+        return false;
+    }
+    
+    public boolean vacia ()
+    {
+        if (raiz == null)
+            return true;
+        else
+            return false;
+    }
+    
+    public void imprimir ()
+    {
+        Nodo reco = raiz;
+        while (reco != null) {
+            System.out.print (reco.info + "-");
+            reco = reco.sig;
+        }
+        System.out.println();
+    }
+	
+}
---
+++ Clase7 Parte 1/src/Lista/ListaTest.java
@@ -0,0 +1,32 @@
+package Lista;
+
+public class ListaTest {
+	public static void main(String[] args) {
+		Lista lg=new Lista();
+	    lg.insertar (1, 10);
+	    lg.insertar (2, 20);
+	    lg.insertar (3, 30);
+	    lg.insertar (2, 15);
+	    lg.insertar (1, 115);
+	    lg.imprimir ();
+	    System.out.println ("Luego de Borrar el primero");
+	    lg.borrar (1);
+	    lg.imprimir ();
+	    System.out.println ("Luego de Extraer el segundo");
+	    lg.extraer (2);
+	    lg.imprimir ();
+	    System.out.println ("Luego de Intercambiar el primero con el tercero");
+	    lg.intercambiar (1, 3);
+	    lg.imprimir ();
+	    if (lg.existe(20)) 
+	        System.out.println("Se encuentra el 20 en la lista");
+	    else
+	        System.out.println("No se encuentra el 20 en la lista");
+	    System.out.println("La posición del mayor es:"+lg.posMayor());
+	    if (lg.ordenada())
+	        System.out.println("La lista está ordenada de menor a mayor");
+	    else
+	        System.out.println("La lista no está ordenada de menor a mayor");    
+	
+	}
+}
